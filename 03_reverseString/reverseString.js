e073e91a5934f728c9d7ea617b2ada7a171298d7
iJoe42 <yts.ijoe.wss@gmail.com>
2023-08-27 05:44:04 +0700

    finished js 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    reverseStr = '';
+
+    for(let i = str.length - 1; i >= 0; i--){
+        reverseStr += str[i]
+    }
+return(reverseStr);
 };
 
 // Do not edit below this line
