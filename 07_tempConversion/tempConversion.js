42e672e0dbe67b4fbb2c2b3c69564b1e43602dfd
iJoe42 <yts.ijoe.wss@gmail.com>
2023-08-27 11:18:50 +0700

    finished js 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+  // convert F to C
+  let tempC = (tempFahr - 32) * 5 / 9;
+
+  // round to 1 decimal
+  return Math.round(tempC*10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCels) {
+  let tempF = (tempCels * 9 / 5) + 32;
+
+  return Math.round(tempF*10)/10;
 };
 
 // Do not edit below this line
