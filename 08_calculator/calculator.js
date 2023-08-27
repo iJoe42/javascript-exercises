ebf0f5b6e2ca40d706e6c1b9c0ff102ec4b891b6
iJoe42 <yts.ijoe.wss@gmail.com>
2023-08-27 12:10:51 +0700

    finished js 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+ let total = 0;
+ for(item of arr){
+  total += item;
+ }
+ return total;
+
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+ let product = 1;
+ args.forEach(multFunc)
+ function multFunc(item){
+  product *= item;
+ }
+ return product;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	return Math.pow(base, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factResult = 1;
+	for(let i = num; i > 1; i--){
+    factResult *= i;
+  }
+  return factResult;
 };
 
 // Do not edit below this line
