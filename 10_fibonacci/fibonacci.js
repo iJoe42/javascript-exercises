b61160c028ff6edc1664917feacd468c7b259c33
iJoe42 <yts.ijoe.wss@gmail.com>
2023-08-27 13:37:14 +0700

    finished js 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if(pos < 0) return "OOPS"
+    let fibSeq = [0,1];
+    for(let i = 1; i < pos; i++){
+        fibSeq.push(fibSeq[i] + fibSeq[i-1]);
+    }
+    return fibSeq[pos];
 };
 
 // Do not edit below this line
