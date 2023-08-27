a5dd774df760c6e13e9f1c2c8cb58a61fcb0d126
iJoe42 <yts.ijoe.wss@gmail.com>
2023-08-27 12:42:29 +0700

    finished js 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return cleanStr === cleanStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
