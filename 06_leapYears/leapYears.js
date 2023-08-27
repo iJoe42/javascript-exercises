bd45f74f50cca2a8f834fbec378b1ef0b27590e9
iJoe42 <yts.ijoe.wss@gmail.com>
2023-08-27 10:53:43 +0700

    finished js 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 4 ==  0){
+        if(year % 100 == 0){
+            if(year % 400 == 0){
+                return(true);
+            }
+            else{
+                return(false);
+            }
+        }
+        else{
+            return(true);
+        }
+    }
+    else{
+        return(false);
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// One-liner
+// leapYears = function(year) { return 
+// ( year % 4 === 0                 &&      (year % 100 !== 0                       ||          year % 400 === 0) 
+//   divisible by 4 is a leapYear   and     (divisible by 100 is not a leapYear     unless      divisible by 400)
+// ); }
