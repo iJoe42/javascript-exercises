7264e0dfb257933b98bb4bf6b9ed044f83345893
iJoe42 <yts.ijoe.wss@gmail.com>
2023-08-27 10:32:07 +0700

    finished js 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if( Number.isInteger(num1) && Number.isInteger(num2) ){
+        let begin = Math.min(num1, num2);
+        let end = Math.max(num1, num2);
+        let resultSum = 0;
+        if(Math.sign(begin) != 1 || Math.sign(end) != 1){
+            return("ERROR");
+        }
+        else{
+            for(let i = begin; i <= end; i++){
+                resultSum += i;
+            }
+            return(resultSum);
+        }
+    }
+    else{
+        return("ERROR");
+    }
 };
 
 // Do not edit below this line
