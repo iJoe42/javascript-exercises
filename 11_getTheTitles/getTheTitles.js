cb0483774a0023d82db85a405e21615b267b07f1
iJoe42 <yts.ijoe.wss@gmail.com>
2023-08-27 14:59:22 +0700

    finished js 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let booksArr = [];
+    for(book of books){
+        booksArr.push(book.title);
+    }
+    return booksArr;
 };
 
 // Do not edit below this line
